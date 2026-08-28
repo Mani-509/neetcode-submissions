548ba8db416f1c206f7d77971e64dedbd79401ec
Mani Shankar V <manishankarvm@gmail.com>
2026-08-28 22:18:05 +0530

    Add: string-matching-in-an-array - submission-3

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        res=[]
+        for ch in words:
+            for i in range(len(words)):
+                if ch in words[i] and ch!=words[i]:
+                    res.append(ch)
+
+        return list(set(res))
